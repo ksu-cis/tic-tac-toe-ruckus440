cfd03fad5858a29e4b2b0aebf5918d0aaf465b8b
Mike Ruckert <mwruckert@gmail.com>
2019-11-13 14:40:44 -0600

    no idea

---
+++ TicTacToe/Pages/ServerTicTacToe.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TicTacToe.Pages
+{
+    public class ServerTicTacToeModel : PageModel
+    {
+        public char Turn { get; set; } = 'x';
+        public char[,] State { get; } = new char[3, 3];
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
